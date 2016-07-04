49029401b57223729586cf2b470841f03c719c08
Marcus <dugginsdaman@yahoo.com>
2016-07-04 16:14:12 +0000

    Simple Validator completed

---
+++ simple-validator/main.js
@@ -0,0 +1,20 @@
+// var tel = prompt("Please enter your Phone Number, IT MIGHT BE SPAMED!, NO PROMISES")
+
+// alert(tel.charAt(3) === "-" && tel.charAt(7) === "-")
+
+// var bday = prompt("Tell me yo berfday?")
+
+// alert(bday.charAt(2) === "/" && bday.charAt(5) === "/")
+
+// var postCode = prompt("What is your Zip Code?")
+
+// alert(postCode.length === 5)
+
+// var state = prompt("Enter the State Abbreviation")
+
+// alert(state.length === 2 && state === state.toUpperCase())
+
+// var married = prompt("Are you married? Yes or No")
+
+// alert(married === "yes" || married === "Yes" || married === "YES" || married === "no" || married === "No" || married === "NO")
+
