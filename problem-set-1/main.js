9e6fb5f03d44c94b9d1cbf79a08cc213c51f72a3
Marcus <dugginsdaman@yahoo.com>
2016-07-04 21:43:49 +0000

    Problem set completed

---
+++ problem-set-1/main.js
@@ -0,0 +1,21 @@
+// var sentence = prompt("Type your sentence here")
+
+// var result = sentence.split(" ")
+// for(var i = 0; i < result.length; i++){
+//     var letter = result[i].split("")
+//     letter[0] = letter[0].toUpperCase()
+//     result[i] = letter.join("")
+// }
+
+// console.log(result.join(" "))
+
+//************************************************
+
+// function wordCount(words){
+//     var result = words.split(" ")
+//     var howMany = result.length
+//     console.log(howMany)
+// }
+
+//************************************************
+
