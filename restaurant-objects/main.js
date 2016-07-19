cdd69ce60d6b20f3f8f9ed0df96b4229422b251f
Marcus <dugginsdaman@yahoo.com>
2016-07-19 17:16:39 +0000

    finished creating objects for part 2 of restaurant assignment

--- restaurant-objects/main.js
+++ restaurant-objects/main.js
@@ -13,3 +13,35 @@ var grilledCheese = new FoodItem('Grilled Cheese', 1500)
 function stringify(foodItem){
     console.log(foodItem.name + ' has ' + foodItem.calories + ' Calories, Vegan: ' + foodItem.vegan + ', Gluten Free: ' + foodItem.glutenFree + ', Citrus Free: ' + foodItem.citrusFree)
 }
+
+function Drink(name,description,price,ingredients){
+    this.name = name;
+    this.desc = description;
+    this.price = price;
+    this.ingredients = [];
+}
+
+function Plate(name,description,price,ingredients){
+    this.name = name;
+    this.desc = description;
+    this.price = price;
+    this.ingredients = [];
+}
+
+function Order(){
+    this.Plates = [];
+}
+
+function Menu(){
+    this.Plates = [];
+}
+
+function Restaurant(){
+    this.name = name;
+    this.desc = description;
+    this.Menu = menu;
+}
+
+function Customer(){
+    this.dietaryPreference = dietaryPreference;
+}
